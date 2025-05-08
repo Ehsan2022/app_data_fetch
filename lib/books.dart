b44b31affa4c9cc2a1ee7cf492d5440392490308
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2025-05-09 00:12:58 +0430

    Initial commit

---
+++ lib/books.dart
@@ -0,0 +1,23 @@
+// Model class
+class Book {
+  final String id;
+  final String name;
+  final String author;
+  final String year;
+
+  Book({
+    required this.id,
+    required this.name,
+    required this.author,
+    required this.year,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+      author: json['author'] ?? '',
+      year: json['year'] ?? '',
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'books.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _bookname = TextEditingController();
+  final TextEditingController _authorname = TextEditingController();
+  final TextEditingController _publishyear = TextEditingController();
+  List<Book> books = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBooks();
+  }
+
+  // Add books to API
+  Future<void> addBook() async {
+    const String apiUrl = 'https://681cd33bf74de1d219adee2a.mockapi.io/books';
+
+    http.post(
+      Uri.parse(apiUrl),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': _bookname.text,
+        'author': _authorname.text,
+        'year': _publishyear.text,
+      }),
+    );
+    _bookname.clear();
+    _authorname.clear();
+    _publishyear.clear();
+    fetchBooks(); // Refresh list after adding
+  }
+
+  // Fetch books from API
+  Future<void> fetchBooks() async {
+    const String apiUrl = 'https://681cd33bf74de1d219adee2a.mockapi.io/books';
+
+    final response = await http.get(Uri.parse(apiUrl));
+    List<dynamic> data = jsonDecode(response.body);
+    setState(() {
+      books = data.map((json) => Book.fromJson(json)).toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Working with API",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              const Text(
+                "Add New Book",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _bookname,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Book Name",
+                  hintText: "Enter Book Name",
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _authorname,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Author Name",
+                  hintText: "Enter Author Name",
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _publishyear,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Publish Year",
+                  hintText: "Enter Book Publish Year",
+                ),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton.icon(
+                onPressed: addBook,
+                label: const Text(
+                  "Add Book",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
+                  padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 18),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  elevation: 4,
+                  shadowColor: Colors.blueGrey.shade700,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Divider(),
+              const Text("Book List", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10),
+              Expanded(
+                child: books.isEmpty
+                    ? const Center(child: Text("No books available."))
+                    : ListView.builder(
+                        itemCount: books.length,
+                        itemBuilder: (context, index) {
+                          final book = books[index];
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2,horizontal: 2),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.blueGrey,
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  child: Text('${index + 1}'),
+                                  ),
+                                title: Text(book.name, style: const TextStyle(color: Colors.white),),
+                                subtitle: Text("Author: ${book.author} | Year: ${book.year}", style: const TextStyle(color: Colors.white70),),
+                                trailing: GestureDetector(
+                                  onTap: (){
+                                    final String deleteUrl = 'https://681cd33bf74de1d219adee2a.mockapi.io/books/${book.id}';
+                                    http.delete(Uri.parse(deleteUrl));
+                                    fetchBooks();
+                                  },
+                                  child: Icon(Icons.delete,color:Colors.white ,size: 28,),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
